47239a09a2787f592180bbe3f7dd37ad413fd744
unknown <bourse.virgile@gmail.com>
2021-11-09 14:35:00 +0100

    gestion des differents cas fonctionnels pour les boutons radio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
@@ -8,13 +8,49 @@ import { FormBuilder, Validators } from '@angular/forms';
 })
 export class AppComponent {
 
+
+  @ViewChild("autreAgeButton") autreAgeButton: ElementRef;
+  @ViewChild("autreDemandeButton") autreDemandeButton: ElementRef;
+
   form = this.fb.group({
     nom: ['',Validators.required],
     email: ['',[Validators.required,Validators.email]],
-    majeur: ['',Validators.required],
+    majeur: [null,[Validators.required]],
+    demande: [null,Validators.required]
     });
 
   constructor(private fb: FormBuilder) {}
+
+  public handleChange() {
+    if(this.form.get("majeur").value == "autre") {
+      console.log("on arrive dans l'autre");
+    } else {
+      console.log("on est pas dans l'autre");
+    }
+    console.log(this.autreAgeButton.nativeElement.checked);
+  }
+
+  /**
+   * 
+   * @returns retourne vrai si le radiobutton "Autre" est selectionné pour le type de demande
+   */
+  public autreDemandeButtonSelected() : boolean {
+    if(this.autreDemandeButton) {
+      return this.autreDemandeButton.nativeElement.checked
+    }
+    return false;
+  }
+
+  /**
+   * 
+   * @returns retourne vrai si le radiobutton "Autre" est selectionné pour l'age
+   */
+  public mineurButtonSelected(): boolean {
+    if(this.autreAgeButton) {
+      return this.autreAgeButton.nativeElement.checked;
+    }
+    return false;
+  }
 
   save() {
   }
