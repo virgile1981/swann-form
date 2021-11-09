a1dc6a276fc31aeaf65118589186110cac798fd7
unknown <bourse.virgile@gmail.com>
2021-11-09 11:11:06 +0100

    mise en place du style css de base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'swann-form';
+
+  form = this.fb.group({
+    nom: [],
+    });
+
+  constructor(private fb: FormBuilder) {}
+
+  save() {}
 }
